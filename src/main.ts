48837b6f52b3691e365ddb9655e83a479aa5cda9
Hubert Serafin <hubis.2001@outlook.com>
2025-03-01 19:16:54 +0100

    Add global API key guard to main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ApiKeyGuard } from 'src/guard/Api-key.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  app.useGlobalGuards(new ApiKeyGuard());
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
