e0c4447e6614dcd2dd85132d0b1b344afcd49855
Hubert Serafin <hubis.2001@outlook.com>
2025-03-01 19:12:48 +0100

    Set application port using environment variable in docker-compose and main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { ApiKeyGuard } from 'src/guard/Api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalGuards(new ApiKeyGuard());
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
