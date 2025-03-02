bcd4fea049c10574f6daaef9b1bc2da0ab2668f2
Hubert Serafin <hubis.2001@outlook.com>
2025-03-02 15:32:47 +0100

    Add HealthGateway provider to HealthModule for WebSocket support

--- src/health/Health.module.ts
+++ src/health/Health.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from 'src/health/Health.controller';
+import { HealthGateway } from 'src/health/Health.gateway';
 
 @Module({
   imports: [TerminusModule],
   controllers: [HealthController],
+  providers: [HealthGateway],
+  exports: [HealthGateway],
 })
 export class HealthModule {}
